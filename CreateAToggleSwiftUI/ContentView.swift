e22f72fcf3d9b3825f4cba8672567a767c7d5983
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-09-26 11:21:04 +0300

    init

--- CreateAToggleSwiftUI/ContentView.swift
+++ CreateAToggleSwiftUI/ContentView.swift
@@ -9,8 +9,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showGrid = false // Set initial state of toggle
     var body: some View {
-        Text("Hello World")
+        ZStack {
+            Toggle(isOn: $showGrid) {
+                HStack {
+                    Image(systemName: "grid")
+                    Text("Show Grid")
+                }
+            }.frame(width: 350)
+                .padding(20)
+                .background(Color(red: 0, green: 0, blue: 1, opacity: 0.2))
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                .offset(y: 362)
+            
+            if showGrid {
+                ZStack {
+                    VStack(alignment: .center, spacing: 34) {
+                        ForEach(0 ..< 24) { item in
+                            Divider()
+                        }
+                    }
+                    HStack(alignment: .center, spacing: 34) {
+                        ForEach(0 ..< 24) { item in
+                            Divider()
+                        }
+                    }
+                }
+            }
+            Capsule()
+                .frame(width: 172, height: 310)
+                .foregroundColor(.pink)
+        }
     }
 }
 
